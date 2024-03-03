56a1cce1e05dfeb1b9f5fb18092215527a15663a
Guilherme S. Magalhaes <silcol455@gmail.com>
2024-03-03 19:25:49 +0000

    Remove unnecessary scripts and update .gitignore

---
+++ sh/pip_install_from_requiremets_txt.sh
@@ -0,0 +1,4 @@
+# Description: Install all packages in requirements.txt
+# Example: sh pip_install_from_requiremets_txt.sh
+
+python3 -m pip install --force-reinstall --no-deps -r requirements.txt
---
+++ start_dev_container.sh
@@ -0,0 +1,13 @@
+sudo chmod -R 777 ./
+
+docker run -it \
+    -v /home/guilherme/.aws:/home/glue_user/.aws \
+    -v ./:/home/glue_user/workspace/ \
+    -v AWS_PROFILE=default \
+    -e DISABLE_SSL=true --rm \
+    -e DATALAKE_FORMATS=iceberg \
+    -p 4040:4040 \
+    -p 18080:18080 \
+    --name glue_pyspark \
+    amazon/aws-glue-libs:glue_libs_4.0.0_image_01 \
+    pyspark
